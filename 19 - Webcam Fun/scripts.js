30244749324f843caf398707db0ca56ac9ed7e7b
Bejamin Schachter <benschac@gmail.com>
2017-01-04 17:19:41 -0500

    finished w/o filters

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,48 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: false
+  })
+  .then(localmediastream => {
+    console.log(localmediastream);
+    video.src = window.URL.createObjectURL(localmediastream);
+    video.play();
+  })
+  .catch(err => console.error(err));
+}
+
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+    let pixels = ctx.getImageData(0, 0, width, height);
+    console.log(pixels);
+    debugger;
+  }, 16);
+}
+
+function takePhoto() {
+  snap.currentTime = 0;
+  snap.play();
+  const data = canvas.toDataURL('image/jpeg');
+
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'handsome');
+  link.innerHTML = `<img src="${data}" alt="person"/>`;
+  strip.insertBefore(link, strip.firstChild);
+
+}
+
+getVideo();
+video.addEventListener('canplay', paintToCanvas);
