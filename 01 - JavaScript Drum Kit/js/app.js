eb38cbddfc2d74d08c2845eb9b07e3021d6acb08
Kamila Tomczyk <ka1130@o2.pl>
2017-01-31 20:50:39 +0100

    Move contents of listener to separate function.

--- 01 - JavaScript Drum Kit/js/app.js
+++ 01 - JavaScript Drum Kit/js/app.js
@@ -1,21 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-	document.addEventListener("keydown", function(event) {
+	function playSound(event) {
 		const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
 		const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
-		if(!audio) return; //stop the function
+		if (!audio) return; //stop the function
 		audio.currentTime = 0; //rewind to the start
 		audio.play();
 		key.classList.add("playing");
-	}, false);
-
-	function removeTransition(event) {
-		
-		if(event.propertyName !== "transform") return; //skip it of it's not a transform
-		this.classList.remove("playing");
 	}
 
-	const keys = document.querySelectorAll(".key");
-	keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+function removeTransition(event) {
 
+	if (event.propertyName !== "transform") return; //skip it of it's not a transform
+	this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key"); keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+
+document.addEventListener("keydown", playSound, false);
 });
