3882dbc81c96432b51b882e53fbfe5ad50d22d9b
Shaun Wahle <shaun.wahle@gmail.com>
2017-11-01 19:33:03 -0700

    Adds function

--- 01 - JavaScript Drum Kit/app.js
+++ 01 - JavaScript Drum Kit/app.js
@@ -3,4 +3,7 @@
 
 window.addEventListener('keydown', function(e){
   console.log(e);
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  if(!audio) return
+  console.log(audio)
 })
