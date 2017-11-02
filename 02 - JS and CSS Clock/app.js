0cf40910211baf1a28fb1fe241169897d32e148f
Shaun Wahle <shaun.wahle@gmail.com>
2017-11-02 12:05:33 -0700

    adds working clock

--- 02 - JS and CSS Clock/app.js
+++ 02 - JS and CSS Clock/app.js
@@ -1,12 +1,20 @@
-const secondHand = document.querySelector('.second-hand')
+const secondHand = document.querySelector('.second-hand');
+const hourHand = document.querySelector('.hour-hand');
+const minHand = document.querySelector('.min-hand');
 
 function setTime() {
   const now = new Date;
-  console.log(secondHand)
   const seconds = now.getSeconds();
   const secondsDegrees = ((seconds / 60) * 360) + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-  console.log(seconds)
+
+  const hours = now.getHours();
+  const hourDegrees = ((seconds / 60) * 360) + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+  const minutes = now.getMinutes();
+  const minDegrees = ((minutes / 60) * 360) + 90;
+  minHand.style.transform = `rotate(${minDegrees}deg)`;
 }
 
 setInterval(setTime, 1000);
