cd9c0a4361d45bb74fbf174be352e8f093a658e9
Shaun Wahle <shaun.wahle@gmail.com>
2017-11-01 19:41:47 -0700

    adds transitions

--- 01 - JavaScript Drum Kit/app.js
+++ 01 - JavaScript Drum Kit/app.js
@@ -2,8 +2,11 @@
 
 
 window.addEventListener('keydown', function(e){
-  console.log(e);
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  if(!audio) return
-  console.log(audio)
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if(!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add('playing')
+  console.log(key);
 })
