682bc69c0122d5e93f4257619a05d6386ed07aa5
MagDry <magdalena.drymajlo@gmail.com>
2017-11-27 14:34:50 +0100

    Day 1 done

--- 01 - JavaScript Drum Kit/app.js
+++ 01 - JavaScript Drum Kit/app.js
@@ -1,26 +1,24 @@
-window.addEventListener('keydown', function(e) {
-    // console.log(e.keyCode);
+function playSound(e) {
 
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"`);
     // console.log(audio);
-    if(!audio) return;
+    if (!audio) return;
 
     audio.currentTime = 0;
     audio.play();
     // console.log(key);
 
     key.classList.add('playing');
-
-});
+};
 
 function removeTransition(e) {
-    if(e.propertyName !== 'transform') return;
+    if (e.propertyName !== 'transform') return;
     // console.log(e.propertyName)
     this.classList.remove('playing');
 }
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-
-
+window.addEventListener('keydown', playSound);
+// console.log(e.keyCode);
