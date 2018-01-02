f164e66cc752fa378e969cb8f705ae82cf58d0b0
remidatroa <remidatroa@gmail.com>
2018-01-02 12:44:21 +0100

    setup canvas video

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -5,7 +5,7 @@ const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
 
 function getVideo() {
-  navigator.mediaDevives.getUserMedia({ video: true, audio: false })
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
     .then(localMediaStream =>{
       console.log(localMediaStream);
       video.src = window.URL.createObjectURL(localMediaStream);
@@ -16,3 +16,27 @@ function getVideo() {
     });
 }
 
+function paintToCanvas() {
+const width = video.videoWidth;
+const height = video.videoHeight;
+canvas.width = width;
+canvas.height = height;
+
+return setInterval(() => {
+  ctx.drawImage(video, 0, 0, width, height);
+  // take the pixels out
+  let pixels = ctx.getImageData(0, 0, width, height);
+  // mess with them
+  // pixels = redEffect(pixels);
+  pixels = rgbSplit(pixels);
+  //ctx.globalAlpha = 0.8;
+
+  // pixels = greenScreen(pixels);
+  // put them back
+  ctx.putImageData(pixels, 0,0);
+ }, 16);
+}
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
