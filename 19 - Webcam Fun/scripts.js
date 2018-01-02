1ea8e622bff41f95beedd0d32a311d895ce09687
remidatroa <remidatroa@gmail.com>
2018-01-02 12:28:50 +0100

    setup to get video

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,16 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevives.getUserMedia({ video: true, audio: false })
+    .then(localMediaStream =>{
+      console.log(localMediaStream);
+      video.src = window.URL.createObjectURL(localMediaStream);
+      video.play();
+    })
+    .catch(err => {
+      console.error(`NUP BRUH!!!`, err);
+    });
+}
+
