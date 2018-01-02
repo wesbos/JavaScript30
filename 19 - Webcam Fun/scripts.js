7bffbbd40df8b33f5574539490f7f3ed13886529
remidatroa <remidatroa@gmail.com>
2018-01-02 13:26:38 +0100

    end on firefox ok redEffect not on chrome

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -6,7 +6,7 @@ const snap = document.querySelector('.snap');
 
 function getVideo() {
   navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-    .then(localMediaStream =>{
+    .then(localMediaStream => {
       console.log(localMediaStream);
       video.src = window.URL.createObjectURL(localMediaStream);
       video.play();
@@ -59,6 +59,43 @@ function redEffect(pixels) {
   }
   return pixels;
 }
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i+= 4) {
+    pixels.data[i - 150] = pixels.data[i + 0]; // red
+    pixels.data[i + 500] = pixels.data[i + 1]; // green
+    pixels.data[i - 550] = pixels.data[i + 2]; // blue
+  }
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+     if (red >= levels.rmin
+        && green >= levels.gmin
+        && blue >= levels.bmin
+        && red <= levels.rmax
+        && green <= levels.gmax
+        && blue <= levels.bmax) {
+        // take it out!
+        pixels.data[i + 3] = 0;
+     }
+  }
+
+  return pixels;
+}
+
 
 getVideo();
 
