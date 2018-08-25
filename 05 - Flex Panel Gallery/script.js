9e9b227f223e1c041baff4ce3647695615743e26
glaszczyk <info@grzegorzlaszczyk.pl>
2018-08-26 00:57:19 +0200

    [js-05] Change CSS to BEM. Add styles for top and bottom texts

--- 05 - Flex Panel Gallery/script.js
+++ 05 - Flex Panel Gallery/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', main);
 
 function main() {
 
-    const toggleOpenClass = (event) => event.currentTarget.classList.toggle('open');
+    const toggleOpenClass = (event) => event.currentTarget.classList.toggle('panel--open');
     const panels = Array.from(document.querySelectorAll('.panel'));
     panels.forEach(panel => panel.addEventListener('click', toggleOpenClass));
 }
