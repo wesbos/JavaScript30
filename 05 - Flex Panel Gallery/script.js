562aade7ecf41f2a058bf7a9acefcb0c5dde17e0
glaszczyk <info@grzegorzlaszczyk.pl>
2018-08-26 00:27:22 +0200

    [js-05] Add event listener to panels

--- 05 - Flex Panel Gallery/script.js
+++ 05 - Flex Panel Gallery/script.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', main);
 
 function main() {
-    console.log('foo');
+
+    const toggleOpenClass = (event) => event.currentTarget.classList.toggle('open');
+    const panels = Array.from(document.querySelectorAll('.panel'));
+    panels.forEach(panel => panel.addEventListener('click', toggleOpenClass));
 }
