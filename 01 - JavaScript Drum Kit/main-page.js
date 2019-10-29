25db32656417287a8c6ec4c0d118ebf0f894aea2
Ashish <rudrashishkumar@gmail.com>
2019-10-29 09:46:42 +0530

    1.Removed <script>from Javascript Drum Kit/index-finished.html and added a seperate javascript file for the page, to keep the html and js code seperate. 2.Corrected spacing and indentations in the html file of Drum kit.

---
+++ 01 - JavaScript Drum Kit/main-page.js
@@ -0,0 +1,19 @@
+function removeTransition(e){
+    if(e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+function playSound(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+    if(!audio) return;
+
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
