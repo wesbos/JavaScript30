79a29d6cbada9af0259f53f1461286cb355dfe62
Alessandro Maclaine <almaclaine@gmail.com>
2020-05-05 16:44:34 -0700

    11 Custom Video Player fix mouseup event
    Make mouseup event on document instead of progress so that the mousedown flag will be reset even if the mouse is released somewhere other than the progress bar. As it stands now if you click drag on the progress bar, move off the bar and release, then mouse down is still activated and scrub will be called whenever the mouse runs over the progress bar.

--- 11 - Custom Video Player/scripts-FINISHED.js
+++ 11 - Custom Video Player/scripts-FINISHED.js
@@ -52,4 +52,4 @@ let mousedown = false;
 progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
 progress.addEventListener('mousedown', () => mousedown = true);
-progress.addEventListener('mouseup', () => mousedown = false);
+document.addEventListener('mouseup', () => mousedown = false);
