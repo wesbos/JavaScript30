bccbadd7b6e8f460aa25143aa3a802094fb731d2
T. Lanette <TLPollard4516@gmail.com>
2020-05-30 14:38:41 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- 01 - JavaScript Drum Kit/Finished Drum Kit/dkscript.js
+++ 01 - JavaScript Drum Kit/Finished Drum Kit/dkscript.js
@@ -1,7 +1,20 @@
-window.addEventListener('keydown', function(e){
+
+function playSound(e){
    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-   console.log(audio);
-   if(!audio) return; //stop the function from running altogether
-   audio.play();
+   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
    
-});
+   if (!audio) return; //stop the function from running altogether
+   audio.currentTime = 0; //rewind to start
+   audio.play();
+   key.classList.add('playing');
+
+}
+
+function removeTransition(e) {
+   if(e.propertyName !== 'transform') return; 
+   this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
