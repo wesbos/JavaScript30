f19230a0084798cf94207b87165a60c29c740d3d
T. Lanette <TLPollard4516@gmail.com>
2020-05-30 13:19:25 -0400

    Update Keydown function
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- 01 - JavaScript Drum Kit/Finished Drum Kit/dkscript.js
+++ 01 - JavaScript Drum Kit/Finished Drum Kit/dkscript.js
@@ -1,4 +1,7 @@
 window.addEventListener('keydown', function(e){
    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
    console.log(audio);
+   if(!audio) return; //stop the function from running altogether
+   audio.play();
+   
 });
