88a1be9449b17c4107fa27e298ee4cad828c2493
林远超 <1726928218@qq.com>
2021-01-26 13:42:55 +0800

    可能完成了？

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,23 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+let startX = 20;
+let startY = 20;
+
+
+function takePhoto() {
+    ctx.fillRect(startX,startY, 1500, 800);
+    canvas.style.opacity = 0;
+    snap.play()
+    let src = canvas.toDataURL("image/png");
+    let a = document.createElement('a');
+    a.setAttribute('download', '');
+    a.setAttribute('href', src);
+    strip.appendChild(a);
+    let img = document.createElement('img')
+    img.setAttribute('src',src);
+    a.appendChild(img);
+}
+
+
+
