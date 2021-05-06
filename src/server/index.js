f73ef841f933df45d5299db45f082ae30ddb7cd3
fre-ben <fschocke@gmail.com>
2021-05-06 13:41:34 +0200

    ✨ Serve static files

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 3000;
 const path = require("path");
 
+app.use(express.static(process.cwd() + "/src/projects/"));
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/index.html"));
 });
