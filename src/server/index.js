bbd8de774dbdfb4687b62324b6a2fd5586fd7931
fre-ben <fschocke@gmail.com>
2021-05-06 12:56:11 +0200

    🎉 Setup JS30 Challenge project Add express and nodemon

---
+++ src/server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.get("/drumkit", (req, res) => {
+  res.sendFile(
+    path.join(
+      process.cwd() + "/src/projects/01 - JavaScript Drum Kit/index-START.html"
+    )
+  );
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
