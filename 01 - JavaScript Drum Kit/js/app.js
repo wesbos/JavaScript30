cb0eab4240b7c8608779bfd139ffb3a50e899f62
Anubis Lockward <anubis.lockward@gmail.com>
2021-07-31 00:57:34 -0400

    Worked in project for First day.

---
+++ 01 - JavaScript Drum Kit/js/app.js
@@ -0,0 +1,37 @@
+window.addEventListener("keydown", keyDownEventListener);
+
+function keyDownEventListener(e) {
+  playSound(e);
+  togglePlayingClass(e);
+}
+
+function playSound({ keyCode }) {
+  const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+
+  if (!audio) {
+    return;
+  }
+
+  audio.currentTime = 0;
+  audio.play();
+}
+
+function togglePlayingClass({ keyCode }) {
+  const key = document.querySelector(`.key[data-key="${keyCode}"]`);
+
+  key.classList.add("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+
+keys.forEach((key) =>
+  key.addEventListener("transitionend", removePlayingClass)
+);
+
+function removePlayingClass(e) {
+  if (e.propertyName !== "transform") {
+    return;
+  }
+
+  this.classList.remove("playing");
+}
