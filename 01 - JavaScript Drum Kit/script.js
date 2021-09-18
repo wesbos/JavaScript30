841f3da72f23554c2379e2098bbb03abfc745710
Kerseee <racs10030746@gmail.com>
2021-09-18 16:17:53 +0800

    Finish practice 01

---
+++ 01 - JavaScript Drum Kit/script.js
@@ -0,0 +1,18 @@
+function playSound(e){
+  const audio = this.document.querySelector(`audio[data-key=${e.key}]`);
+  const key = this.document.querySelector(`.key[data-key=${e.key}]`);
+  if(!audio) return;
+  audio.currentTime = 0;    // 沒加這行的話無法快速重播聲音
+  audio.play();
+  key.classList.add('playing');
+}
+
+function removeTransition(e){
+  if(e.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+}
+
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
