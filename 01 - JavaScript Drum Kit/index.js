a3997369ac2f004b16c09fb5232701e42ca682c8
1ntegralan0maly <theonewhocodes10@gmail.com>
2022-06-07 12:55:07 +0200

    created independent function to play sound

--- 01 - JavaScript Drum Kit/index.js
+++ 01 - JavaScript Drum Kit/index.js
@@ -1,11 +1,12 @@
-window.addEventListener('keydown', function (e) {
+
+function playSound (e) {
   const audio = this.document.querySelector(`audio[data-key = ${e.code}]`);
   const key = this.document.querySelector(`.key[data-key = ${e.code}]`);
   if (!audio) return; // stops function from running when unasigned key is pressed
   audio.currentTime = 0; // rewinds audio to start upon secondary key press
   audio.play();
   key.classList.toggle('playing');
-});
+}
 
 function toggleTransition(e) {
   if (e.propertyName !== 'transform') return; // skips events that are not propertyName 'transform'
@@ -14,3 +15,4 @@ function toggleTransition(e) {
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', toggleTransition));
+window.addEventListener('keydown', playSound);
