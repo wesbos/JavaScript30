fe5b4697e9edfdfde91f1c3f370eae19653bf03a
1ntegralan0maly <theonewhocodes10@gmail.com>
2022-06-07 12:38:58 +0200

    added function to end animation after key press

--- 01 - JavaScript Drum Kit/index.js
+++ 01 - JavaScript Drum Kit/index.js
@@ -6,3 +6,11 @@ window.addEventListener('keydown', function (e) {
   audio.play();
   key.classList.toggle('playing');
 });
+
+function toggleTransition(e) {
+  if (e.propertyName !== 'transform') return; // skips events that are not propertyName 'transform'
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', toggleTransition));
