9d5e24178fe497bd89c57512c3f9a29937c98248
Renaud Van Meerbergen <renaud.vanmeerbergen@gmail.com>
2022-12-19 16:35:50 +0100

    Can't do it for now ❌

---
+++ 06 - Type Ahead/main.js
@@ -0,0 +1,3 @@
+/*----------------- Renaud Vmb -----------------*/
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
