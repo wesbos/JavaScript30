e071b8f03611511928426aa6cb98ab23b7c732d5
Renaud Van Meerbergen <renaud.vanmeerbergen@gmail.com>
2022-12-19 12:02:35 +0100

    Fifth Exercice Done ✅

---
+++ 05 - Flex Panel Gallery/main.js
@@ -0,0 +1,12 @@
+/*----------------- Renaud Vmb -----------------*/
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach(panel => panel.addEventListener('click', (e) => {
+    e.currentTarget.classList.toggle('open');
+}));
+
+panels.forEach(panel => panel.addEventListener('transitionend', (e) => {
+    if (e.propertyName.includes('flex')) {
+        e.currentTarget.classList.toggle('open-active');
+    }
+}));
