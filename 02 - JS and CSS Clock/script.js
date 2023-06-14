92fe04b4ec6c15f8cd2b3b301f832b8cbb3ebf5a
GeorgeLennon <130295939+GeorgeLennon@users.noreply.github.com>
2023-06-14 14:31:50 +0100

    Day 02 Complete

---
+++ 02 - JS and CSS Clock/script.js
@@ -0,0 +1,23 @@
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const mins = now.getMinutes();
+  const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;//to account for the 0.2s transition
+  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+  const hour = now.getHours();
+  const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
+
+setDate();//set interval, is like a window event listener for time
