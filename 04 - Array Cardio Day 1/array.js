3e8cfedcf649c7806d31c1d38e8179ee8b38ca6a
tan-sahil <tha.sahu@gmail.com>
2023-07-28 07:22:30 -0700

    conribution of 4 days

---
+++ 04 - Array Cardio Day 1/array.js
@@ -0,0 +1,18 @@
+
+// sort the name of the people from People array
+const people = [
+    'Bernhard, Sandra', 'Bethea, Erin', 'Becker, Carl', 'Bentsen, Lloyd', 'Beckett, Samuel', 'Blake, William', 'Berger, Ric', 'Beddoes, Mick', 'Beethoven, Ludwig',
+    'Belloc, Hilaire', 'Begin, Menachem', 'Bellow, Saul', 'Benchley, Robert', 'Blair, Robert', 'Benenson, Peter', 'Benjamin, Walter', 'Berlin, Irving',
+    'Benn, Tony', 'Benson, Leana', 'Bent, Silas', 'Berle, Milton', 'Berry, Halle', 'Biko, Steve', 'Beck, Glenn', 'Bergman, Ingmar', 'Black, Elk', 'Berio, Luciano',
+    'Berne, Eric', 'Berra, Yogi', 'Berry, Wendell', 'Bevan, Aneurin', 'Ben-Gurion, David', 'Bevel, Ken', 'Biden, Joseph', 'Bennington, Chester', 'Bierce, Ambrose',
+    'Billings, Josh', 'Birrell, Augustine', 'Blair, Tony', 'Beecher, Henry', 'Biondo, Frank'
+  ];
+
+
+  const sortedNames = people.sort((first, second) =>{
+    const [P1firstName, P1lastName] = first.split(",");
+    const [P2firstName, P2lastName] = second.split(",");
+    return P1firstName > P2firstName ? -1: 1;
+  })
+
+  console.log(sortedNames);
